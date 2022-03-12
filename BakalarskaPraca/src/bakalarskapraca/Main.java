41e4150617578feb78969d0e78cf965bcead4230
Bl0ccky <tomas.labat1@gmail.com>
2022-03-12 22:12:29 +0100

    Ahoj

--- BakalarskaPraca/src/bakalarskapraca/Main.java
+++ BakalarskaPraca/src/bakalarskapraca/Main.java
@@ -4,7 +4,7 @@ public class Main
 {
     public static void main(String[] args)
     {
-        //DataLoader dataLoader = new DataLoader("D:\\Vysoká škola\\5. semester\\Bakalarka\\gtfs - Krakow");
+        DataLoader dataLoader = new DataLoader("D:\\Vysoká škola\\5. semester\\Bakalarka\\gtfs - Krakow");
         //dataLoader.getAllStops();
         //dataLoader.getAllRoutes();
         //dataLoader.getAllCalendars();
